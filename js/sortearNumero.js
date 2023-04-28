22ad32de0ba8478f5d9454260f8aabb48a31a988
Renan Lucena <renan.segura.de.lucena@gmail.com>
2023-04-28 15:22:25 -0300

    Add files via upload

---
+++ js/sortearNumero.js
@@ -0,0 +1,19 @@
+const menorValor=1;
+const maiorValor=1000;
+
+
+
+const numeroSecreto = geraNumeroAleatorio();
+
+function geraNumeroAleatorio(){
+    return parseInt(Math.random()*maiorValor+1);
+}
+
+console.log('Numero Secreto:', numeroSecreto);
+
+const elementoMenorValor = document.getElementById('menor-valor');
+elementoMenorValor.innerHTML = menorValor;
+
+
+const elementoMaiorValor = document.getElementById('maior-valor');
+elementoMaiorValor.innerHTML = maiorValor;
